10c143ced3ddd351d96a4945f178bd524c0a7e6a
HyejuChoi <124635547+HyejuChoi@users.noreply.github.com>
2024-03-28 22:42:38 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import ToggleTheme from '@/components/toggle-theme';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col px-12">
+      <h1 className="dark:text-gray-100 my-12 font-bold">Dark Mode App</h1>
+      <ToggleTheme />
+    </main>
+  );
+}
